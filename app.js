7ae586ec101940eae9be8f3ec53dc4fbb40972b5
Shigerubt <z648s.7bt@gmail.com>
2025-01-27 21:49:18 -0600

    Add validation for adding and drawing friends in secret friend functionality

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 let amigoSecreto = [];
 
-
 function agregarAmigo() {
     let amigo = document.getElementById('amigo').value;
-    amigoSecreto.push(amigo);
+    if (amigo == '') {
+        alert('No puedes agregar un amigo vacío');
+    } else {
+        amigoSecreto.push(amigo);
+        document.getElementById('amigo').value = '';
+        document.getElementById('listaAmigos').innerHTML = amigoSecreto.join(', ');
+    }   
 }
 
 function sortearAmigo() {
-    let amigoSorteado = amigoSecreto[Math.floor(Math.random() * amigoSecreto.length)];
-    document.getElementById('sorteado').innerHTML = amigoSorteado;
-    eliminarAmigo();
-}
+    let validar = amigoSecreto.length;
+    if (validar == 0) {
+        alert('No hay amigos para sortear');
+    } else {
+        let amigoSorteado = amigoSecreto[Math.floor(Math.random() * amigoSecreto.length)];
+        document.getElementById('resultado').innerHTML = amigoSorteado;        
 
-function eliminarAmigo() {
-    amigoSecreto.pop();
+        let index = amigoSecreto.indexOf(amigoSorteado);
+        if (index > -1) {
+            amigoSecreto.splice(index, 1);
+        }
+
+        document.getElementById('listaAmigos').innerHTML = amigoSecreto.join(', ');
+
+        if (amigoSecreto.length == 0) {
+            alert('Se acabaron los amigos a sortear, inicia de nuevo');
+        }
+    }
 }
